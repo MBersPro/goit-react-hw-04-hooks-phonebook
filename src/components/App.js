aa3c5d35768fc60e8755ff17481f776a0a426bb4
MBers <mbers.pro@gmail.com>
2021-11-11 01:30:20 +0300

    Done

--- src/components/App.js
+++ src/components/App.js
@@ -5,8 +5,8 @@ import ContactForm from "./contactForm/ContactForm";
 import ContactList from "./contactList/ContactList";
 import Filter from "./filter/Filter";
 
-const App = () => {
-  const [contacts, setContacts] = useState(
+const defaultChooser = () => {
+  return (
     JSON.parse(localStorage.getItem("contacts")) || [
       { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
       { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
@@ -14,21 +14,12 @@ const App = () => {
       { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
     ]
   );
+};
+
+const App = () => {
+  const [contacts, setContacts] = useState(defaultChooser());
   const [filter, setFilter] = useState("");
 
-  // state = {
-  //   contacts: JSON.parse(localStorage.getItem("contacts")) || [
-  //           { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
-  //           { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
-  //           { id: "id-3", name: "Eden Clements", number: "645-17-79" },
-  //           { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
-  //         ],
-  //   filter: "",
-  // };
-
-  // componentDidUpdate(prevProps, prevState) {
-  //   localStorage.setItem("contacts", JSON.stringify(this.state.contacts))
-  // }
   useEffect(() => {
     localStorage.setItem("contacts", JSON.stringify(contacts));
   }, [contacts]);
